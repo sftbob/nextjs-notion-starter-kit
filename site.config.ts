a3821db61c22d1f3ea4e7792d969408fcae930e2
sftbob <111048941+sftbob@users.noreply.github.com>
2024-12-19 01:30:07 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,11 +2,11 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '160304513d05809e9163e604631f146e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: 'fde5ac74-eea3-4527-8f00-4482710e1af3',
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
